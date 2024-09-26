65a094d69503e03c41a0c7c42982f38840d3a2d1
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2024-09-26 12:31:39 +0200

    add stasis simulation for pre-paleoTS format

---
+++ tests/testthat/test_stasis_sl.R
@@ -0,0 +1,8 @@
+test_that("correct S3 class is returned",{
+  expect_s3_class(stasis_sl(1:2), "pre_paleoTS")
+})
+
+test_that("incorrect parameters are caught", {
+  expect_error(stasis_sl(1:2, interpop_var = 0))
+  expect_error(stasis_sl(1:2, n_per_sample = 0))
+})
