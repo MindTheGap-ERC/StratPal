6a50f85cf0e3eb1ab13dc47ceaea4ece45b2b9b6
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2024-10-03 08:45:09 +0200

    add OU process for pre-paleoTS

--- R/ornstein_uhlenbeck_sl.R
+++ R/ornstein_uhlenbeck_sl.R
@@ -1,31 +1,33 @@
-random_walk_sl = function(t, sigma = 1, mu = 0, y0 = 0, interpop_var = 1, n_per_sample = 10){
+ornstein_uhlenbeck_sl = function(t, mu = 0, theta = 1, sigma = 1, y0 = 0, interpop_var = 1, n_per_sample = 10){
+
   #' @export
   #'
-  #' @title simulate random walk (pre-paleoTS format)
+  #' @title simulate ornstein-uhlenbeck (OU) process (pre-paleoTS format)
   #'
-  #' @param t numeric vector with strictly increasing elements, can be heterodistant. Times at which the random walk is evaluated
-  #' @param sigma positive scalar, variance parameter
-  #' @param mu scalar, directionality parameter
-  #' @param y0 scalar, starting value (value of the random walk at the first entry of `t`)
+  #' @param t times at which the process is simulated. Can be heterodistant
+  #' @param mu scalar, long term mean
+  #' @param theta scalar, mean reversion speed
+  #' @param sigma positive scalar, strength of randomness
+  #' @param y0 scalar, initial value (value of process at the first entry of t)
   #' @param interpop_var interpopulation variance, determines how many specimens from the same population vary
   #' @param n_per_sample integer, number of specimens sampled per population/sampling locality
   #'
   #' @description
-    #'  Simulates a (continuous time) random walk as a Brownian drift on specimen level
-    #'
-  #'
+  #' Simulates an Ornstein-Uhlenbeck process on specimen level (_sl). The mean trait value is simulated using the Euler-Maruyama method. The process is simulated on a scale of `0.25 * min(diff(t))` and then interpolated to the values of `t`. At each sampling location there are `n_per_sample` specimens that are normally distributed around the mean trait value with a variance of `interpop_var`.
   #'
   #' @returns an object of S3 class `pre_paleoTS`, inherits from `timelist` and `list`. The list has two elements: `t`, containing a vector of times of sampling, and `vals`, a list of trait values of the same length as `t`, with element containing trait values of individual specimens.  This object can be transformed using `apply_taphonomy`, `apply_niche` or `time_to_strat`, and then reduced to a `paleoTS` object using `reduce_to_paleoTS`. This can then be used to test for different modes of evolution.
   #'
-  #' @seealso [random_walk()] for the equivalent function to simulate mean trait values
+  #' @seealso [ornstein_uhlenbeck()] to model only mean trait values
   #'
+
   if (interpop_var <= 0){
     stop("parameter \'interpop_var\' must me >0 ")
   }
   if (n_per_sample <1){
     stop("parameter \'n_per_sample\' must be >=1")
   }
-  x = random_walk(t, sigma, mu, y0)
+
+  x = ornstein_uhlenbeck(t, sigma, mu, y0)
   r = list(t = x$t)
   vals = list()
   for (i in seq_along(x$t)){
---
+++ tests/testthat/test_ornstein_uhlenbeck_sl.R
@@ -0,0 +1,8 @@
+test_that("works without problems", {
+  expect_no_condition(ornstein_uhlenbeck_sl(1:3))
+})
+
+test_that("incorrect parameters are caught", {
+  expect_error(ornstein_uhlenbeck_sl(1:2, interpop_var = 0))
+  expect_error(ornstein_uhlenbeck_sl(1:2, n_per_sample = 0))
+})
