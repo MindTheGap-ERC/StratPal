96f7db1df27047aeda33835c14944b34661f2caf
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2024-10-14 14:16:04 +0200

    bugfix in apply niche and taphonomy, correct typos

--- R/apply_niche.R
+++ R/apply_niche.R
@@ -81,7 +81,7 @@ apply_niche.pre_paleoTS = function(x, niche_def, gc){
   thin_vals = change_in_niche(x$t)
   for (i in seq_along(thin_vals)){
     r = prob_remove(x$vals[[i]], prob = thin_vals[i])
-    x$vals[[i]] = x$vals[[i]][as.logical(r)]
+    x$vals[[i]] = r
   }
   return(x)
 }
