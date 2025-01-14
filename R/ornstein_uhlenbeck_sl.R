1bf92f3d14f716460c1094360cf864a40ca564ca
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2025-01-14 10:15:52 +0100

    explain differences in parametrization

--- R/ornstein_uhlenbeck_sl.R
+++ R/ornstein_uhlenbeck_sl.R
@@ -14,6 +14,11 @@ ornstein_uhlenbeck_sl = function(t, mu = 0, theta = 1, sigma = 1, y0 = 0, intrap
   #'
   #' @description
   #' Simulates an Ornstein-Uhlenbeck process on specimen level (_sl). The mean trait value is simulated using the Euler-Maruyama method. The process is simulated on a scale of `0.25 * min(diff(t))` and then interpolated to the values of `t`. At each sampling location there are `n_per_sample` specimens that are normally distributed around the mean trait value with a variance of `intrapop_var`.
+  #'  Note that different parametrizations of OU processes are used in the literature. Here we use the parametrization common in mathematics. This translates to the parametrization used in evolutionary biology (specifically, the one in Hansen (1997)) as follows:
+  #'  * `sigma` is identical
+  #'  * `mu` used in the `StratPal` package corresponds to `theta` sensu Hansen (1997)
+  #'  * `theta` as used in the `StratPal` package corresponds to `alpha` sensu Hansen (1997)
+  #'
   #'
   #' @returns an object of S3 class `pre_paleoTS`, inherits from `timelist` and `list`. The list has two elements: `t`, containing a vector of times of sampling, and `vals`, a list of trait values of the same length as `t`, with element containing trait values of individual specimens.  This object can be transformed using `apply_taphonomy`, `apply_niche` or `time_to_strat`, and then reduced to a `paleoTS` object using `reduce_to_paleoTS`. This can then be used to test for different modes of evolution.
   #'
